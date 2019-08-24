28206fb466223b674ecec55f3c51f386c5a72448
imed jaberi <imed_jebari@hotmail.fr>
2019-08-24 18:54:22 +0200

    update unit test using ` mocha ☕️ ` & ` assert native nodejs 🤠 ` 👻 ..

--- test/password-meter-test.js
+++ test/password-meter-test.js
@@ -77,193 +77,193 @@ console.log
 
 describe('password meter test - faible' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('12345678',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,0);
-          });
+     it ('Condition 1', () => {
+          let result = package ('12345678',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,0);
+     });
      
-          it ('Condition 2', () => {
-               let result = package ('imedjaberi',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,0);
-          });
+     it ('Condition 2', () => {
+          let result = package ('imedjaberi',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,0);
+     });
      
-          it ('Condition 3', () => {
-               let result = package ('IMEDJABERI',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,0);
-          });
+     it ('Condition 3', () => {
+          let result = package ('IMEDJABERI',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,0);
+     });
      
-          it ('Condition 4', () => {
-               let result = package ('+°%/*-+-',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,0);
-          });
+     it ('Condition 4', () => {
+          let result = package ('+°%/*-+-',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,0);
+     });
      
-          it ('Condition 5', () => {
-               let result = package ('+°%/ * +-',{ displayString : false , useSpace : true } ) ; 
-               assert.equal(result,0);
-          });
+     it ('Condition 5', () => {
+          let result = package ('+°%/ * +-',{ displayString : false , useSpace : true } ) ; 
+          assert.equal(result,0);
+     });
 
-          it ('Condition 6', () => {
-                    let result = package ('12345678',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FAIBLE');
-          });
+     it ('Condition 6', () => {
+          let result = package ('12345678',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FAIBLE');
+     });
           
-          it ('Condition 7', () => {
-                    let result = package ('imedjaberi',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FAIBLE');
-          });
+     it ('Condition 7', () => {
+          let result = package ('imedjaberi',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FAIBLE');
+     });
           
-          it ('Condition 8', () => {
-                    let result = package ('IMEDJABERI',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FAIBLE');
-          });
+     it ('Condition 8', () => {
+          let result = package ('IMEDJABERI',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FAIBLE');
+     });
           
-          it ('Condition 9', () => {
-                    let result = package ('+°%/*-+-',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FAIBLE');
-          });
+     it ('Condition 9', () => {
+          let result = package ('+°%/*-+-',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FAIBLE');
+     });
           
-          it ('Condition 10', () => {
-                    let result = package ('+°%/ * +-',{ displayString : true , useSpace : true } ) ; 
-                    assert.equal(result,'FAIBLE');
-          });
+     it ('Condition 10', () => {
+          let result = package ('+°%/ * +-',{ displayString : true , useSpace : true } ) ; 
+          assert.equal(result,'FAIBLE');
+     });
 });
 
 describe('password meter test - AVG' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123456im',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,1);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123456im',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,1);
+     });
      
-          it ('Condition 2', () => {
-               let result = package ('123IM456',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,1);
-          });
+     it ('Condition 2', () => {
+          let result = package ('123IM456',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,1);
+     });
      
-          it ('Condition 3', () => {
-               let result = package ('imedJABERI',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,1);
-          });
+     it ('Condition 3', () => {
+          let result = package ('imedJABERI',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,1);
+     });
 
-          it ('Condition 4', () => {
-                    let result = package ('123456im',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'AVG');
-          });
+     it ('Condition 4', () => {
+          let result = package ('123456im',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'AVG');
+     });
           
-          it ('Condition 5', () => {
-                    let result = package ('123IM456',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'AVG');
-          });
+     it ('Condition 5', () => {
+          let result = package ('123IM456',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'AVG');
+     });
           
-          it ('Condition 6', () => {
-                    let result = package ('imedJABERI',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'AVG');
-          });
+     it ('Condition 6', () => {
+          let result = package ('imedJABERI',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'AVG');
+     });
 });
 
 describe('password meter test - ABOVE AVG' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123456*+',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,2);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123456*+',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,2);
+     });
      
-          it ('Condition 2', () => {
-               let result = package ('IMED CSM',{ displayString : false , useSpace : true } ) ; 
-               assert.equal(result,2);
-          });
+     it ('Condition 2', () => {
+          let result = package ('IMED CSM',{ displayString : false , useSpace : true } ) ; 
+          assert.equal(result,2);
+     });
      
-          it ('Condition 3', () => {
-               let result = package ('i+love+my+mom',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,2);
-          });
+     it ('Condition 3', () => {
+          let result = package ('i+love+my+mom',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,2);
+     });
 
-          it ('Condition 4', () => {
-                    let result = package ('123456*+',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'ABOVE AVG');
-          });
+     it ('Condition 4', () => {
+          let result = package ('123456*+',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'ABOVE AVG');
+     });
           
-          it ('Condition 5', () => {
-                    let result = package ('IMED CSM',{ displayString : true , useSpace : true } ) ; 
-                    assert.equal(result,'ABOVE AVG');
-          });
+     it ('Condition 5', () => {
+          let result = package ('IMED CSM',{ displayString : true , useSpace : true } ) ; 
+          assert.equal(result,'ABOVE AVG');
+     });
           
-          it ('Condition 6', () => {
-                    let result = package ('i+love+my+mom',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'ABOVE AVG');
-          });
+     it ('Condition 6', () => {
+          let result = package ('i+love+my+mom',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'ABOVE AVG');
+     });
 });
 
 
 describe('password meter test - STRONG AVG' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123S56im',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,3);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123S56im',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,3);
+     });
 
-          it ('Condition 2', () => {
-                    let result = package ('123S56im',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'STRONG AVG');
-          });
+     it ('Condition 2', () => {
+          let result = package ('123S56im',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'STRONG AVG');
+     });
 });
 
 
 describe('password meter test - FORT' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123+56im',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,4);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123+56im',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,4);
+     });
 
-          it ('Condition 2', () => {
-                    let result = package ('123S56+M',{ displayString : false , useSpace : false } ) ; 
-                    assert.equal(result,4);
-          });
+     it ('Condition 2', () => {
+          let result = package ('123S56+M',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,4);
+     });
 
-          it ('Condition 3', () => {
-                    let result = package ('imed+csM',{ displayString : false , useSpace : false } ) ; 
-                    assert.equal(result,4);
-          });
+     it ('Condition 3', () => {
+          let result = package ('imed+csM',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,4);
+     });
      
-          it ('Condition 4', () => {
-                    let result = package ('123+56im',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FORT');
-               });
+     it ('Condition 4', () => {
+          let result = package ('123+56im',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FORT');
+     });
      
-          it ('Condition 5', () => {
-                    let result = package ('123S56+M',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FORT');
-          });
+     it ('Condition 5', () => {
+          let result = package ('123S56+M',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FORT');
+     });
      
-          it ('Condition 6', () => {
-                    let result = package ('imed+csM',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'FORT');
-          });
+     it ('Condition 6', () => {
+          let result = package ('imed+csM',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'FORT');
+     });
 });
 
 describe('password meter test - ABOVE FORT' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123+5S6m',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,5);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123+5S6m',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,5);
+     });
 
-          it ('Condition 2', () => {
-                    let result = package ('123+5S6m',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'ABOVE FORT');
-          });
+     it ('Condition 2', () => {
+          let result = package ('123+5S6m',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'ABOVE FORT');
+     });
 });
 
 describe('password meter test - STRONG FORT' , () => {
 
-          it ('Condition 1', () => {
-               let result = package ('123+5S6im',{ displayString : false , useSpace : false } ) ; 
-               assert.equal(result,6);
-          });
+     it ('Condition 1', () => {
+          let result = package ('123+5S6im',{ displayString : false , useSpace : false } ) ; 
+          assert.equal(result,6);
+     });
 
-          it ('Condition 2', () => {
-                    let result = package ('123+5S6im',{ displayString : true , useSpace : false } ) ; 
-                    assert.equal(result,'STRONG FORT');
-          });
+     it ('Condition 2', () => {
+          let result = package ('123+5S6im',{ displayString : true , useSpace : false } ) ; 
+          assert.equal(result,'STRONG FORT');
+     });
 });
