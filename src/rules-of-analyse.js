d4ad313193ba8720effb7d09bced168421f09eed
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-15 22:00:23 +0100

    clean-up the core code 🎯 ..

--- src/rules-of-analyse.js
+++ src/rules-of-analyse.js
@@ -1,21 +1,22 @@
 const rules = (TailleAnalysis ) => {
-  let rules = new Object() ;
-  if ( TailleAnalysis > 9 ) {
-        rules = { 
-                  firstRule : parseInt( TailleAnalysis * 0.3 ) , 
-                  secondRule : parseInt( TailleAnalysis * 0.1 ) ,
-                  thirdRule : parseInt( TailleAnalysis * 0.1) 
-        };
+  let rules = {};
+
+  if (TailleAnalysis > 9) {
+    rules = { 
+      firstRule : parseInt( TailleAnalysis * 0.3 ), 
+      secondRule : parseInt( TailleAnalysis * 0.1 ),
+      thirdRule : parseInt( TailleAnalysis * 0.1) 
+    };
   }else{
-        rules = { 
-                  firstRule : parseInt( TailleAnalysis * 0.3 ) , 
-                  secondRule : parseInt( TailleAnalysis * 0.2 ) ,
-                  thirdRule : parseInt( TailleAnalysis * 0.2)
-                };
+    rules = { 
+      firstRule : parseInt( TailleAnalysis * 0.3 ), 
+      secondRule : parseInt( TailleAnalysis * 0.2 ),
+      thirdRule : parseInt( TailleAnalysis * 0.2)
+    };
   };
 
-        return rules ;
+  return rules ;
 };
 
 
-module.exports = rules ;
+module.exports = rules;
