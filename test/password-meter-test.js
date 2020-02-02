5c2ed19fdd984a53e567acf06ec373838e712006
3imed-jaberi <imed_jebari@hotmail.fr>
2020-02-02 23:12:14 +0100

    update test with new feature 2.x.x ☕️ ..

--- test/password-meter-test.js
+++ test/password-meter-test.js
@@ -1,42 +1,40 @@
-require('mocha');
 const assert = require('assert');
-const package = require('../index'); 
+const package = require('..'); 
 
 
 
 let result;
 
-
 describe('password meter test - error', () => {
 
   it ('Condition 1', () => {
     result = package ('123456'); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
   });
 
   it ('Condition 2', () => {
     result = package (''); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
    });
 
   it ('Condition 3', () => {
     result = package ('12345678 '); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
   });
 
   it ('Condition 4', () => {
     result = package (' '); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
   });
 
   it ('Condition 5', () => {
     result = package ('   ', { useSpace : true }); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
   });
 
   it ('Condition 6', () => {
     result = package ('12345678910111112424242525', { useSpace : true }); 
-    assert.equal(result,-1);
+    assert.equal(result, -1);
   });
 
   it ('Condition 7', () => {
@@ -72,67 +70,56 @@ describe('password meter test - error', () => {
 });
 
 
-console.log
-(`
-          *************************
-          *  WELCOME TO THE TEST  * 
-          *************************
-          \n 
- !! => SPACE is special chart  .. 
-
-`);
-
-
 describe('password meter test - faible', () => {
 
   it ('Condition 1', () => {
     result = package ('12345678'); 
-    assert.equal(result,0);
+    assert.equal(result, 0);
   });
      
   it ('Condition 2', () => {
     result = package ('imedjaberi'); 
-    assert.equal(result,0);
+    assert.equal(result, 0);
   });
      
   it ('Condition 3', () => {
     result = package ('IMEDJABERI'); 
-    assert.equal(result,0);
+    assert.equal(result, 0);
   });
      
   it ('Condition 4', () => {
     result = package ('+°%/*-+-'); 
-    assert.equal(result,0);
+    assert.equal(result, 0);
   });
      
   it ('Condition 5', () => {
     result = package ('+°%/ * +-', { useSpace : true }); 
-    assert.equal(result,0);
+    assert.equal(result, 0);
   });
 
   it ('Condition 6', () => {
     result = package ('12345678', { displayString : true }); 
-    assert.equal(result,'FAIBLE');
+    assert.equal(result, 'FAIBLE');
   });
           
   it ('Condition 7', () => {
     result = package ('imedjaberi', { displayString : true }); 
-    assert.equal(result,'FAIBLE');
+    assert.equal(result, 'FAIBLE');
   });
           
   it ('Condition 8', () => {
     result = package ('IMEDJABERI', { displayString : true }); 
-    assert.equal(result,'FAIBLE');
+    assert.equal(result, 'FAIBLE');
   });
           
   it ('Condition 9', () => {
     result = package ('+°%/*-+-', { displayString : true }); 
-    assert.equal(result,'FAIBLE');
+    assert.equal(result, 'FAIBLE');
   });
           
   it ('Condition 10', () => {
     result = package ('+°%/ * +-', { displayString : true , useSpace : true }); 
-    assert.equal(result,'FAIBLE');
+    assert.equal(result, 'FAIBLE');
   });
 
 });
@@ -142,32 +129,32 @@ describe('password meter test - AVG' , () => {
 
   it ('Condition 1', () => {
     result = package ('123456im'); 
-    assert.equal(result,1);
+    assert.equal(result, 1);
   });
      
   it ('Condition 2', () => {
     result = package ('123IM456'); 
-    assert.equal(result,1);
+    assert.equal(result, 1);
   });
      
   it ('Condition 3', () => {
     result = package ('imedJABERI'); 
-    assert.equal(result,1);
+    assert.equal(result, 1);
   });
 
   it ('Condition 4', () => {
     result = package ('123456im', { displayString : true }); 
-    assert.equal(result,'AVG');
+    assert.equal(result, 'AVG');
   });
           
   it ('Condition 5', () => {
     result = package ('123IM456', { displayString : true }); 
-    assert.equal(result,'AVG');
+    assert.equal(result, 'AVG');
   });
           
   it ('Condition 6', () => {
     result = package ('imedJABERI', { displayString : true }); 
-    assert.equal(result,'AVG');
+    assert.equal(result, 'AVG');
   });
 
 });
@@ -177,32 +164,32 @@ describe('password meter test - ABOVE AVG' , () => {
 
   it ('Condition 1', () => {
     result = package ('123456*+'); 
-    assert.equal(result,2);
+    assert.equal(result, 2);
   });
      
   it ('Condition 2', () => {
     result = package ('IMED CSM', { useSpace : true }); 
-    assert.equal(result,2);
+    assert.equal(result, 2);
   });
      
   it ('Condition 3', () => {
     result = package ('i+love+my+mom'); 
-    assert.equal(result,2);
+    assert.equal(result, 2);
   });
 
   it ('Condition 4', () => {
     result = package ('123456*+', { displayString : true }); 
-    assert.equal(result,'ABOVE AVG');
+    assert.equal(result, 'ABOVE AVG');
   });
           
   it ('Condition 5', () => {
     result = package ('IMED CSM', { displayString : true , useSpace : true }); 
-    assert.equal(result,'ABOVE AVG');
+    assert.equal(result, 'ABOVE AVG');
   });
           
   it ('Condition 6', () => {
     result = package ('i+love+my+mom', { displayString : true }); 
-    assert.equal(result,'ABOVE AVG');
+    assert.equal(result, 'ABOVE AVG');
   });
 
 });
@@ -212,12 +199,12 @@ describe('password meter test - STRONG AVG' , () => {
 
   it ('Condition 1', () => {
     result = package ('123S56im'); 
-    assert.equal(result,3);
+    assert.equal(result, 3);
   });
 
   it ('Condition 2', () => {
     result = package ('123S56im', { displayString : true }); 
-    assert.equal(result,'STRONG AVG');
+    assert.equal(result, 'STRONG AVG');
   });
 
 });
@@ -227,32 +214,32 @@ describe('password meter test - FORT' , () => {
 
   it ('Condition 1', () => {
     result = package ('123+56im'); 
-    assert.equal(result,4);
+    assert.equal(result, 4);
   });
 
   it ('Condition 2', () => {
     result = package ('123S56+M'); 
-    assert.equal(result,4);
+    assert.equal(result, 4);
   });
 
   it ('Condition 3', () => {
     result = package ('imed+csM'); 
-    assert.equal(result,4);
+    assert.equal(result, 4);
   });
      
   it ('Condition 4', () => {
     result = package ('123+56im', { displayString : true }); 
-    assert.equal(result,'FORT');
+    assert.equal(result, 'FORT');
   });
      
   it ('Condition 5', () => {
     result = package ('123S56+M', { displayString : true }); 
-    assert.equal(result,'FORT');
+    assert.equal(result, 'FORT');
   });
      
   it ('Condition 6', () => {
     result = package ('imed+csM', { displayString : true }); 
-    assert.equal(result,'FORT');
+    assert.equal(result, 'FORT');
   });
 
 });
@@ -262,12 +249,12 @@ describe('password meter test - ABOVE FORT' , () => {
 
   it ('Condition 1', () => {
     result = package ('123+5S6m'); 
-    assert.equal(result,5);
+    assert.equal(result, 5);
   });
 
   it ('Condition 2', () => {
     result = package ('123+5S6m', { displayString : true }); 
-    assert.equal(result,'ABOVE FORT');
+    assert.equal(result, 'ABOVE FORT');
   });
 
 });
@@ -277,12 +264,12 @@ describe('password meter test - STRONG FORT' , () => {
 
   it ('Condition 1', () => {
     result = package ('123+5S6im'); 
-    assert.equal(result,6);
+    assert.equal(result, 6);
   });
 
   it ('Condition 2', () => {
     result = package ('123+5S6im', { displayString : true }); 
-    assert.equal(result,'STRONG FORT');
+    assert.equal(result, 'STRONG FORT');
   });
 
 });
