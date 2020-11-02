0acc927383fe016db4a6e2b36de670dde47b0c4b
3imed-jaberi <imed_jebari@hotmail.fr>
2020-11-02 22:31:16 +0100

    add some todos :D' ! ..

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
-const { StringOpt, IntOpt } = require('./outputOpts.json');
+// ___ Constants ___ //
+// Output Options
+const STRING_OPTS = [
+  "ERROR", "FAIBLE", "AVG", "ABOVE AVG",
+  "STRONG AVG",  "FORT", "ABOVE FORT", "STRONG FORT"
+]
+const INPUT_OPTS = [-1, 0, 1, 2, 3, 4, 5, 6]
 
-// ___ Constants ___ // 
-// RegExp Pattern .. //
-const lowerCaseLetters = /[a-z]/g;
-const upperCaseLetters = /[A-Z]/g;
-const numbers = /[0-9]/g ;
-const specialChars = /\W/g;
-const espaceChars =  /\s/g;
-
+// RegExp Pattern
+const lowerCaseLetters = /[a-z]/g
+const upperCaseLetters = /[A-Z]/g
+const numbers = /[0-9]/g 
+const specialChars = /\W/g
+const espaceChars =  /\s/g
 
 // ___ Private Use ___ //
+// EspaceSearch and result according to the developer choice ...
+const espaceCharAnalysisFunc = (password, useSpace) => useSpace ? null : password.match(espaceChars)
 
-// espaceSearch and result according to the developer choice ...
-const espaceCharAnalysisFunc = (password, useSpace) => useSpace ? null : password.match(espaceChars);
+// Get the rules collection ..
+const getRules = TailleAnalysis => {
+  // getRulesForShortPassword : 0.1
+  // getRulesForLongPassword : 0.2
+  const eps = TailleAnalysis > 9 ? 0.1 : 0.2
 
-// get the rules collection ..
-const getRules = (TailleAnalysis ) => { 
-
-  const getRulesForShortPassword = () => ({
-    firstRule : parseInt(TailleAnalysis * 0.3), 
-    secondRule : parseInt(TailleAnalysis * 0.1),
-    thirdRule : parseInt(TailleAnalysis * 0.1) 
-  });
-
-  const getRulesForLongPassword = () => ({
-    firstRule : parseInt(TailleAnalysis * 0.3), 
-    secondRule : parseInt(TailleAnalysis * 0.2),
-    thirdRule : parseInt(TailleAnalysis * 0.2)
-  });
-
-  return TailleAnalysis > 9 ? getRulesForShortPassword() : getRulesForLongPassword();
-};
-
+  return {
+    firstRule : parseInt(TailleAnalysis * 0.3),
+    secondRule : parseInt(TailleAnalysis * eps),
+    thirdRule : parseInt(TailleAnalysis * eps)
+  }
+}
 
 // check the rules .. 
-const checkRules = (displayOpts) => {
-
-  
+const checkRules = displayOpts => {
   let { password, useSpace } = displayOpts,
   // number of characters in the password ...
   TailleAnalysis = password.length, 
@@ -45,73 +40,115 @@ const checkRules = (displayOpts) => {
   // searche all null(s) results .. 
   nullResultSearch = MyAnalysisResult.filter(unit => unit === null),
 
-  espaceCharAnalysis = espaceCharAnalysisFunc(password, useSpace);
+  espaceCharAnalysis = espaceCharAnalysisFunc(password, useSpace)
 
   // 8 --> 24 support .. || espaceCharAnalysis: just for fix useSpace .. 
-  if(TailleAnalysis > 24 || TailleAnalysis < 8 || espaceCharAnalysis !== null){
-    return -1;
-  }
+  if (
+    TailleAnalysis > 24 ||
+    TailleAnalysis < 8 ||
+    espaceCharAnalysis !== null
+  ) return -1
 
   switch (nullResultSearch.length) {
     case 0: {
-      let { firstRule, secondRule, thirdRule } = getRules(TailleAnalysis);
+      let { firstRule, secondRule, thirdRule } = getRules(TailleAnalysis)
       // Fort mais Trés fort ou non ! 
       return (
-        lowerCaseAnalysis.length >= firstRule
-        ?
-        (
+        lowerCaseAnalysis.length >= firstRule ?
           upperCaseAnalysis.length >=  secondRule 
-          ?
-          (specialCharAnalysis.length >=  thirdRule ? 6 : null) 
-          : 
-          null 
-        )
-        :
-        5
-      );
+            ? (specialCharAnalysis.length >=  thirdRule ? 6 : null) 
+            : null 
+          : 5
+      )
     }
     case 1: {
-      return specialCharAnalysis === null ? 3 : 4;
+      return specialCharAnalysis === null ? 3 : 4
     }
     case 2: {
-      return specialCharAnalysis === null ? 1 : 2;
+      return specialCharAnalysis === null ? 1 : 2
     }
     case 4: {
-      return -1;
+      return -1
     }
     default: {
-      return 0;
+      return 0
     }
   }
-};
+}
 
-
-// find all patterns inside the password .. 
+// find all patterns inside the password
 const searchResult = password => ([
   lowerCaseAnalysis = password.match(lowerCaseLetters),
   upperCaseAnalysis = password.match(upperCaseLetters),
   numberAnalysis = password.match(numbers),
   specialCharAnalysis = password.match(specialChars)
-]);
-
+])
 
 // ___ Public Funcs ___ //
+const pMeter = (password, opts = {}) => {
+  const numberResult = checkRules({ password, useSpace: (opts.useSpace || false) })
+  const stringResult = STRING_OPTS[INPUT_OPTS.findIndex(value => value === numberResult)]
 
-const pMeter = (password, pOpts) => {
-  let Opts = pOpts || {},
-  displayString = Opts.displayString || false, 
-  useSpace = Opts.useSpace || false,
-  intResult = checkRules({ password, useSpace }),
-  stringResult = StringOpt[IntOpt.findIndex(value => value === intResult)];
-
-  return displayString ? stringResult : intResult;
+  return (opts.displayString || false) ? stringResult : numberResult
 }
 
+module.exports = pMeter
 
+// TODO
+// ESLINT
+// USE NYC OVER ISTANBUL
+// ERROR TYPES --code
+// CHECK RULES --code
+// MAKE TEST CODE OUTSIDE THE TEST DIR
+// TRAVIS CI PIPELINE
+// REVIEW README.md FILE
+// ***** LAST RELEASE ******
 
-module.exports = pMeter;
+// SOME CODE *****
+// error types
+  // if (nullResultSearch.length === 4) {
+  //   return ' : empty password';
+  // }else if (espaceCharAnalysis !== null) {
+  //   return ' : useSpace is not active';
+  // }else if ((8 > TailleAnalysis) || (TailleAnalysis > 24)) {
+  //   return ' : validate the length of password';
+  // }else {
+  //   return ''; 
+  // };
 
+// checkRules -- simple way
+  // const validationRules = (displayOpts) => {
 
+  //   let { nullResultSearch, espaceChartAnalysis, TailleAnalysis } = displayOpts;
 
-// error types >>> note.txt .. 
-// checkRules -- simple way >>> note.txt
+  //   if ((nullResultSearch.length === 4) || (espaceChartAnalysis !== null) || (TailleAnalysis > 24) || (TailleAnalysis < 8)){
+  //     return -1;
+  //   }else if (nullResultSearch.length === 0) {
+  //     let myRules = rules(TailleAnalysis);
+  //     let myRules = getRules(TailleAnalysis);
+  //     // Fort mais Trés fort ou non ! 
+  //     if (lowerCaseAnalysis.length >= myRules.firstRule) {
+  //       if (upperCaseAnalysis.length >=  myRules.secondRule) {
+  //         if ( specialChartAnalysis.length >=  myRules.thirdRule ) {
+  //           return 6;
+  //         }
+  //       }
+  //     }else {
+  //       return  5;
+  //     }
+  //   }else if (nullResultSearch.length === 1) {
+  //     if (specialChartAnalysis === null) {
+  //       return 3;
+  //     }else {
+  //       return 4;
+  //     }
+  //   }else if (nullResultSearch.length === 2){
+  //     if (specialChartAnalysis === null) {
+  //       return 1;
+  //     }else {            
+  //       return 2;
+  //     }
+  //   }else{
+  //     return 0;
+  //   };
+  // };
